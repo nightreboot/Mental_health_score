84fa152d7ad43d9610f26512d1c03310b214768f
Raj Vishwakarma <rv422196@gmail.com>
2026-07-26 13:34:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ form.addEventListener("submit", async (e) => {
   };
 
   try {
-    const res = await fetch(API_URL, {
+    const res = await fetch(`${API_URL}/predict`{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
